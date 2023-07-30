608055091cf96b7c958265caf2c8b9a676cadb20
Hans Steffens <hans@steffens.co>
2023-07-29 19:36:42 -0500

    Payment Info Sectio completed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ Treehouse Techdegree:
 FSJS Project 3 - Interactive Form
 */
 
-// Creating and Selecting elements
+// Creating variables for selecting elements globally.
 const userName = document.getElementById("name");
 const JobRole = document.getElementById("title");
 const otherJobRole = document.getElementById("other-job-role");
@@ -11,18 +11,13 @@ const color = document.getElementById("color");
 const design = document.getElementById("design");
 const activities = document.getElementById('activities');
 const activitiesCost = document.getElementById('activities-cost');
-let total = 0;
 
-/**
- * Hide the "text field" with the id of "other-job-role" so it is not displayed when the form first loads.
- * When the page first loads, focus the input element for the "Name" field and disabled the color <select> element.
- */
-
-window.addEventListener("load", () => {
-    userName.focus();
-    otherJobRole.style.display = "none";
-    color.disabled = true; // Disable the color `<select>` element on page load.
-});
+// When the page loads, focus the first form field: the input element for the "Name" field.
+userName.focus();
+// Hide the "text field" with the id of "other-job-role" so it is not displayed when the form loads.
+otherJobRole.style.display = "none";
+// Disable the color `<select>` element on page load.
+color.disabled = true;
 
 /**
  * Evvent listener to listen for changes on `Job Role` <select> element.
@@ -44,7 +39,7 @@ JobRole.addEventListener("change", () => {
  * - The "Color" dropdown shows only the color options associated with the selected design.
  */
 
-const shirtColors = (e) => { 
+const shirtColors = (e) => {
     const colors = color.children;
     const selected = color.firstElementChild;
 
@@ -70,6 +65,9 @@ const shirtColors = (e) => {
  * of the selected activities in the "Register for Activities" section.
  */
 
+// The total cost of the activities variable with an initial value of 0.
+let total = 0;
+
 const totalCost = (e) => {
     let activityCost = +(e.target.getAttribute('data-cost'));
 
@@ -81,6 +79,28 @@ const totalCost = (e) => {
     activitiesCost.innerHTML = `Total: $${total}`;
 }
 
+// Set variables to reference the payment methods elements available.
+const payment = document.getElementById("payment");
+const creditCard = document.getElementById("credit-card");
+const payPal = document.getElementById("paypal");
+const bitCoin = document.getElementById("bitcoin");
+
+// Hide the following payment methods initially upon page load.
+paypal.style.display = "none";
+bitCoin.style.display = "none";
+
+// "Credit Card" payment method selected for the user by default.
+payment.firstElementChild.nextElementSibling.setAttribute("selected", "");
+
+// This `paymentMethod` arrow function hides all payment sections in the form’s UI except the selected one.
+const paymentMethod = (e) => {
+    const payWith = e.target.value;
+    paypal.style.display = "none";
+    bitCoin.style.display = "none";
+    creditCard.style.display = "none";
+    document.getElementById(payWith).style.display = "block";
+}
+
 // Event Handlers
 
 // Listener for updating the total cost in real time as the user check or uncheck activities
@@ -88,3 +108,6 @@ activities.addEventListener("change", totalCost);
 
 // Listener to enable the `Color` <select> element when a `Design Theme` is selected.
 design.addEventListener("change", shirtColors);
+
+// Listener to display the payment method selected by the user.
+payment.addEventListener("change", paymentMethod);
